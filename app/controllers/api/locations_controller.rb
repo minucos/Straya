d1d30bc21e7acc3396cbf8b210fade0dfc791256
Andrew Minucos <minucos@gmail.com>
2019-02-28 19:42:48 -0800

    Remove Location controller to fix heroku

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -1,23 +1,23 @@
 class Api::LocationsController < ApplicationController
 
-    def new
-        @location = Location.new
-    end 
+    # def new
+    #     @location = Location.new
+    # end 
 
-    def create
-        @location = Location.new(location_params)
+    # def create
+    #     @location = Location.new(location_params)
 
-        if @location.save
-            render :json @location
-        else
-            render json: @location.errors.full_messages, status: 422 
-        end 
-    end 
+    #     if @location.save
+    #         render :json @location
+    #     else
+    #         render json: @location.errors.full_messages, status: 422 
+    #     end 
+    # end 
 
-    private
+    # private
 
-    def location_params
-        params.require(:location).permit(:lat, :lng, :creator_id, :ord)
-    end 
+    # def location_params
+    #     params.require(:location).permit(:lat, :lng, :creator_id, :ord)
+    # end 
 
 end
