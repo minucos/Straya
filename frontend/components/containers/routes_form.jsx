86e2d56b9c66c273694fae9746538e6cabf8ba89
Andrew Minucos <minucos@gmail.com>
2019-03-08 12:46:42 -0800

    Add redirect to route show page on create

--- frontend/components/containers/routes_form.jsx
+++ frontend/components/containers/routes_form.jsx
@@ -100,7 +100,7 @@ class RoutesForm extends React.Component {
         
 
         this.props.action(newRoute, this.state.locations )
-        .then(() => this.props.history.push("/athlete/routes"))
+            .then((payload) => this.props.history.push(`/routes/${payload.route.id}`))
     };
 
     render() {
