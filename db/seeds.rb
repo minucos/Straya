31879d35e64886e60e9d9701a9b1648b1b1f9532
Andrew Minucos <minucos@gmail.com>
2019-11-24 10:29:53 -0800

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 require 'faker'
 require 'open-uri'
+require 'date'
 
 ActiveRecord::Base.transaction do
 
@@ -77,44 +78,31 @@ end
 #Workouts
 p 'creating workouts...'
 workout_types = ['run', 'ride']
+athlete_ids = [andy.id, holly.id, toby.id]
+dates = (0..180).to_a
 
-300.times do |n|
-    athlete_id = [andy.id, holly.id, toby.id].sample
-    title = Faker::TvShows::Simpsons.location
-    body = Faker::TvShows::Simpsons.quote
-    workout_type = workout_types.sample
+350.times do |n|
     distance = workout_type == 'run' ? rand(5.0..15.0).round(1) : rand(20.0..45.0).round(1)
     duration = workout_type == 'run' ? (distance * 300) + rand(-100..100) : (distance * 120) + rand(-20..20)
-    Workout.create!(title: title, body: body, workout_type: workout_type, duration: duration, distance: distance, athlete_id: athlete_id)
+    Workout.create!({
+        title: Faker::TvShows::Simpsons.location, 
+        body: Faker::TvShows::Simpsons.quote, 
+        workout_type: workout_types.sample, 
+        duration: duration, 
+        distance: distance, 
+        athlete_id: athlete_ids.sample, 
+        created_at: (DateTime.now - dates.sample).to_s
+    })
 end
 
-workout1 = Workout.create!(title: "Gotta start somewhere", body: "nice night for it", workout_type: "run", duration: 1800, distance: 5, athlete_id: andy.id)
-workout2 = Workout.create!(title: "new year, new me... or something", body: "winter bods aren't THAT bad, you know", workout_type: "run", duration: 1796, distance: 6.1, athlete_id: holly.id)
-workout3 = Workout.create!(title: "Running is great!", body: "I especially like when they are over", workout_type: "run", duration: 825, distance: 2.7, athlete_id: toby.id)
-workout4 = Workout.create!(title: "Some pain, some gain", body: "Have to get faster", workout_type: "run", duration: 4127, distance: 14.1, athlete_id: andy.id)
-workout5 = Workout.create!(title: "Lots of pain, lots of gain!", body: "Pretty happy with this pace", workout_type: "run", duration: 5398, distance: 21.1, athlete_id: andy.id)
-workout6 = Workout.create!(title: "A quick run is a good run", body: "so looks like I need to start running faster", workout_type: "run", duration: 1532, distance: 5, athlete_id: holly.id)
-workout7 = Workout.create!(title: "just put one paw in front of the other", body: "Now that is over, where are my dentastix", workout_type: "run", duration: 2003, distance: 5, athlete_id: toby.id)
-workout8  = Workout.create!(title: "10K used to seem so far", body: "Onwards and upwards to a new PB", workout_type: "run", duration: 2500, distance: 10, athlete_id: andy.id)
-
-#Route
+#Routes
+p 'creating routes...'
 route1 = Route.create!(title: "My favourite route", description: "home to astro's", creator_id: toby.id)
-
-#Workouts
-workout9 = Workout.create!(title: "run around the park", body: "They told me thats where we were going", workout_type: "run", duration: 1800, distance: 5, athlete_id: toby.id)
-workout10 = Workout.create!(title: "Another run", body: "Hammering those hills", workout_type: "run", duration: 3627, distance: 12, athlete_id: andy.id)
-
-#Route
 route2 = Route.create!(title: "Morning dog run", description: "walk to silver towers", creator_id: andy.id)
-
-#Workouts
-workout11 = Workout.create!(title: "5k time trial", body: "Not a bad effort if I say so myself!", workout_type: "run", duration: 1734, distance: 5, athlete_id: holly.id)
-workout12 = Workout.create!(title: "Up to the dog park", body: "quick trip to the park to see my mates", workout_type: "run", duration: 400, distance: 0.7, athlete_id: toby.id)
-
-#Route
 route3 = Route.create!(title: "Run to Central Park", description: "up to central park", creator_id: holly.id)
 
 # Locations
+p 'creating locations...'
 #route 1
 r1loc1 = Location.create!(lat: 40.759129, lng: -73.995859, route_id: route1.id, ord: 1 )
 r1loc2 = Location.create!(lat: 40.757917, lng: -73.996762, route_id: route1.id, ord: 2 )
